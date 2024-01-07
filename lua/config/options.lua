5e585628eb0256315f85159ebd4d9e0a486e2253
gianlucapiccirillo <gianluca.piccirillo@mail.mcgill.ca>
2024-01-06 20:36:28 -0500

    init

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,8 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+--
+
+vim.o.tabstop = 4 -- Number of spaces tabs count for
+vim.o.softtabstop = 4 -- Number of spaces in tab when editing
+vim.o.shiftwidth = 4 -- Number of spaces used for auto-indenting
