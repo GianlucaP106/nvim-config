0d83bde36f354cd6f4b5cffcace22716de20ccce
gianlucapiccirillo <gianluca.piccirillo@mail.mcgill.ca>
2024-01-06 22:18:45 -0500

    dap ui

---
+++ lua/plugins/dap-ui-config.lua
@@ -0,0 +1,17 @@
+return {
+    "rcarriga/nvim-dap-ui",
+    config = function(_, opts)
+        -- setup dap config by VsCode launch.json file
+        -- require("dap.ext.vscode").load_launchjs()
+        local dap = require("dap")
+        local dapui = require("dapui")
+        dapui.setup(opts)
+        dap.listeners.after.event_initialized["dapui_config"] = function()
+            dapui.open({})
+        end
+        dap.listeners.before.event_terminated["dapui_config"] = function() end
+        dap.listeners.before.event_exited["dapui_config"] = function() end
+
+        vim.fn.sign_define("DapBreakpoint", { text = "🔴", texthl = "red", linehl = "", numhl = "" })
+    end,
+}
