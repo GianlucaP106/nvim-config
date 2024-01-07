19671429c57a0202126f779c516538a77d7aac42
gianlucapiccirillo <gianluca.piccirillo@mail.mcgill.ca>
2024-01-07 14:48:44 -0500

    dap config

--- lua/plugins/dap-ui-config.lua
+++ lua/plugins/dap-ui-config.lua
@@ -9,7 +9,7 @@ return {
         dap.listeners.after.event_initialized["dapui_config"] = function()
             dapui.open({})
         end
-        dap.listeners.before.event_terminated["dapui_config"] = function() end
-        dap.listeners.before.event_exited["dapui_config"] = function() end
+        -- dap.listeners.before.event_terminated["dapui_config"] = function() end
+        -- dap.listeners.before.event_exited["dapui_config"] = function() end
     end,
 }
