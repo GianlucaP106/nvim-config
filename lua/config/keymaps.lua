c10d009dd64e6390df6944a69889c8dc988e8cf9
gianlucapiccirillo <gianluca.piccirillo@mail.mcgill.ca>
2024-01-13 18:45:14 -0500

    telescope fix

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,12 +1,4 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 
--- fuzz search in current buffer
-vim.keymap.set("n", "<leader>.", function()
-    require("telescope.builtin").current_buffer_fuzzy_find(require("telescope.themes").get_dropdown({
-        winblend = 10,
-        previewer = false,
-    }))
-end, { desc = "Fuzzily search in current buffer" })
-
 -- quit window key map
 vim.keymap.set("n", "<leader>qw", ":q<CR>", { desc = "Quit window" })
--- lua/plugins/telescope-config.lua
+++ lua/plugins/telescope-config.lua
@@ -1,27 +1,45 @@
 return {
-  -- change some telescope options and a keymap to browse plugin files
-  {
-    "nvim-telescope/telescope.nvim",
-    keys = {
-      -- add a keymap to browse plugin files
-      -- stylua: ignore
-      {
-        "<leader>fp",
-        function() require("telescope.builtin").find_files({ cwd = require("lazy.core.config").options.root }) end,
-        desc = "Find Plugin File",
-      },
-    },
-  },
+    -- change some telescope options and a keymap to browse plugin files
+    {
+        "nvim-telescope/telescope.nvim",
+        keys = {
+            -- add a keymap to browse plugin files
+            -- stylua: ignorecase
 
-  -- add telescope-fzf-native
-  {
-    "telescope.nvim",
-    dependencies = {
-      "nvim-telescope/telescope-fzf-native.nvim",
-      build = "make",
-      config = function()
-        require("telescope").load_extension("fzf")
-      end,
+            {
+                "<leader>fp",
+                function()
+                    require("telescope.builtin").find_files({ cwd = require("lazy.core.config").options.root })
+                end,
+                desc = "Find Plugin File",
+            },
+            { -- override this keymap to fix a bug
+                "<leader>ff",
+                ":Telescope find_files<CR>",
+                desc = "Find Files",
+            },
+            {
+                "<leader>.",
+                function()
+                    require("telescope.builtin").current_buffer_fuzzy_find(require("telescope.themes").get_dropdown({
+                        winblend = 10,
+                        previewer = false,
+                    }))
+                end,
+                desc = "Fuzzily search in current buffer",
+            },
+        },
     },
-  },
+
+    -- add telescope-fzf-native
+    {
+        "telescope.nvim",
+        dependencies = {
+            "nvim-telescope/telescope-fzf-native.nvim",
+            build = "make",
+            config = function()
+                require("telescope").load_extension("fzf")
+            end,
+        },
+    },
 }
