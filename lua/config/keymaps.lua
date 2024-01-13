8ae073e8149266cfa4749a90ac4aaad7e3ed935d
gianlucapiccirillo <gianluca.piccirillo@mail.mcgill.ca>
2024-01-12 21:19:21 -0500

    keymap

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,7 +1,6 @@
--- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
 
+-- fuzz search in current buffer
 vim.keymap.set("n", "<leader>.", function()
     -- You can pass additional configuration to telescope to change theme, layout, etc.
     require("telescope.builtin").current_buffer_fuzzy_find(require("telescope.themes").get_dropdown({
@@ -9,3 +8,6 @@ vim.keymap.set("n", "<leader>.", function()
         previewer = false,
     }))
 end, { desc = "Fuzzily search in current buffer" })
+
+-- quit window key map
+vim.keymap.set("n", "<leader>qw", ":q<CR>", { desc = "Quit window" })
