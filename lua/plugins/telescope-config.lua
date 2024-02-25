5d5e7b1f00b7a2b0b7ed1c472e820be6b8483dcb
gianlucapiccirillo <gianluca.piccirillo@mail.mcgill.ca>
2024-02-25 15:36:54 -0500

    clean telescope config

--- lua/plugins/telescope-config.lua
+++ lua/plugins/telescope-config.lua
@@ -1,39 +1,13 @@
-local find_files = function()
-    require("telescope.builtin").find_files({
-        hidden = true,
-        file_ignore_patterns = {
-            ".git",
-        },
-    })
-end
-
-local find_file_config = function(key)
-    return {
-        key,
-        find_files,
-        desc = "Find Files",
-    }
-end
-
 return {
-    -- change some telescope options and a keymap to browse plugin files
+    -- add current buffer search
     {
         "nvim-telescope/telescope.nvim",
         keys = {
-            -- {
-            --     "<leader>fp",
-            --     function()
-            --         require("telescope.builtin").find_files({ cwd = require("lazy.core.config").options.root })
-            --     end,
-            --     desc = "Find Plugin File",
-            -- },
-            -- find_file_config("<leader>ff"),
-            -- find_file_config("<leader><space>"),
             {
                 "<leader>.",
                 function()
                     require("telescope.builtin").current_buffer_fuzzy_find(require("telescope.themes").get_dropdown({
-                        winblend = 10,
+                        -- winblend = 10,
                         previewer = false,
                     }))
                 end,
