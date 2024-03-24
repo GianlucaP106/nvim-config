22ae81079afaeb08776baa946752569bd7e50673
Gianluca Piccirillo <gianluca.piccirillo@mail.mcgill.ca>
2024-03-24 17:23:07 -0400

    fix: java lombok

--- lua/plugins/lazy-extras.lua
+++ lua/plugins/lazy-extras.lua
@@ -3,7 +3,11 @@ return {
     { import = "lazyvim.plugins.extras.dap.core" },
     { import = "lazyvim.plugins.extras.editor.outline" },
     { import = "lazyvim.plugins.extras.lang.clangd" },
-    { import = "lazyvim.plugins.extras.lang.java" },
+
+    -- for some reason java needs to be installed the other way (using the LazyExtras interface).
+    -- Discovered on march 24th 2024 - noticed lombok was not working
+    -- { import = "lazyvim.plugins.extras.lang.java" },
+    --
     { import = "lazyvim.plugins.extras.lang.json" },
     { import = "lazyvim.plugins.extras.lang.markdown" },
     { import = "lazyvim.plugins.extras.lang.python" },
