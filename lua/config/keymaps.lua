8c9363c14c29e65c7567dcbf6dec9f3b9ec87140
Gianluca Piccirillo <gianluca.piccirillo@mail.mcgill.ca>
2024-04-15 17:37:20 -0400

    remap to avoid using arrow

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,3 +2,20 @@
 
 -- quit window key map
 vim.keymap.set("n", "<leader>qw", ":q<CR>", { desc = "Quit window" })
+
+-- forces me to use hjkl
+vim.keymap.set("n", "<Down>", function()
+    print("use j")
+end, { desc = "" })
+
+vim.keymap.set("n", "<Up>", function()
+    print("use k")
+end, { desc = "" })
+
+vim.keymap.set("n", "<Left>", function()
+    print("use h")
+end, { desc = "" })
+
+vim.keymap.set("n", "<Right>", function()
+    print("use l")
+end, { desc = "" })
