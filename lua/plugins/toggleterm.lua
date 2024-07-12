c8316c27f7d34e26842677e0bf0ec11641ebb5f9
Gianluca Piccirillo <gianluca.piccirillo@mail.mcgill.ca>
2024-07-12 17:29:52 -0400

    fix toggleterm mapping for which-key v3

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -26,8 +26,8 @@ return {
     {
         "folke/which-key.nvim",
         opts = {
-            defaults = {
-                ["<leader>h"] = { name = "+Toggle termninal" },
+            spec = {
+                { "<leader>h", group = "Toggle termninal" },
             },
         },
     },
