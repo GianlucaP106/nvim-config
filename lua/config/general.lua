dfead7a32a48ef6c61715da38da77e12cfebb454
Gianluca Piccirillo <gianluca.piccirillo@mail.mcgill.ca>
2025-01-14 22:27:07 -0500

    lazy.nvim float ui config

--- lua/config/general.lua
+++ lua/config/general.lua
@@ -1,5 +1,8 @@
 -- General config loaded after lazy bootstrap (manually added)
 
+-- general
+vim.api.nvim_set_hl(0, "FloatBorder", { bg = "#000000" })
+
 -- config which key styles
 vim.api.nvim_set_hl(0, "WhichKeyNormal", { bg = "#000000" })
 vim.api.nvim_set_hl(0, "WhichKeyBorder", { bg = "#000000", fg = "#808080" })
@@ -10,3 +13,6 @@ vim.api.nvim_set_hl(0, "SnacksIndentScope", { fg = "#A0A0A0" })
 
 -- fzf-lua highlights
 vim.api.nvim_set_hl(0, "FzfLuaDirPart", { link = "NonText" })
+
+-- lazy.nvim highlights
+vim.api.nvim_set_hl(0, "LazyNormal", { bg = "#000000" })
