be09dfabad8ad61cfd243d6a9d48ccca9031dabb
Gianluca Piccirillo <gianluca.piccirillo@mail.mcgill.ca>
2025-01-16 20:39:11 -0500

    bufferline colors configuration

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -1,3 +1,4 @@
+-- local bufferline = require("bufferline")
 return {
     "akinsho/bufferline.nvim",
     opts = {
@@ -13,8 +14,118 @@ return {
             background = {
                 link = "NonText",
             },
-            buffer_visible = {
+            fill = {
                 link = "NonText",
+            },
+            trunc_marker = {
+                link = "NonText",
+            },
+            group_separator = {
+                link = "BufferLineSeparator",
+            },
+
+            buffer = {
+                link = "NonText",
+            },
+            buffer_visible = {
+                link = "BufferLineBuffer",
+            },
+
+            tab = {
+                link = "NonText",
+            },
+            tab_close = {
+                link = "BufferLineTab",
+            },
+
+            numbers = {
+                link = "NonText",
+            },
+            numbers_visible = {
+                link = "BufferLineNumbers",
+            },
+
+            diagnostic = {
+                link = "NonText",
+            },
+            diagnostic_visible = {
+                link = "BufferLineDiagnostic",
+            },
+
+            duplicate = {
+                fg = "#ff8700",
+                italic = true,
+            },
+            duplicate_visible = {
+                link = "BufferLineDuplicate",
+            },
+            duplicate_selected = {
+                fg = "#ff8700",
+                italic = true,
+                bold = true,
+            },
+
+            close_button = {
+                fg = "#1f1f1f",
+            },
+            close_button_visible = {
+                link = "BufferLineCloseButton",
+            },
+
+            error = {
+                fg = "#f44747",
+                italic = true,
+            },
+            error_visible = {
+                link = "BufferLineError",
+            },
+            error_diagnostic = {
+                link = "BufferLineError",
+            },
+            error_diagnostic_visible = {
+                link = "BufferLineError",
+            },
+
+            info = {
+                fg = "#569cd6",
+                italic = true,
+            },
+            info_visible = {
+                link = "BufferLineInfo",
+            },
+            info_diagnostic = {
+                link = "BufferLineInfo",
+            },
+            info_diagnostic_visible = {
+                link = "BufferLineInfo",
+            },
+
+            warning = {
+                fg = "#a5a57f",
+                italic = true,
+            },
+            warning_visible = {
+                link = "BufferLineWarning",
+            },
+            warning_diagnostic = {
+                link = "BufferLineWarning",
+            },
+            warning_diagnostic_visible = {
+                link = "BufferLineWarning",
+            },
+
+            hint = {
+                fg = "#569cd6",
+                italic = true,
+            },
+            hint_visible = {
+                link = "BufferLineHint",
+            },
+            hint_diagnostic = {
+                link = "BufferLineHint",
+            },
+            hint_diagnostic_visible = {
+                link = "BufferLineHint",
             },
         },
     },
