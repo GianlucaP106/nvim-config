840e372637f93f8f5a2e00fe39c6abfb4f0ccb2f
Gianluca Piccirillo <gianluca.piccirillo@mail.mcgill.ca>
2025-07-31 14:50:57 -0400

    renabled mistakingly disabled nvim-lspconfig

--- lua/plugins/clangd.lua
+++ lua/plugins/clangd.lua
@@ -1,11 +1,11 @@
 return {
-    "neovim/nvim-lspconfig",
-    enabled = false,
-    opts = {
-        servers = {
-            clangd = {
-                mason = false,
-            },
-        },
-    },
+    -- "neovim/nvim-lspconfig",
+    -- enabled = false,
+    -- opts = {
+    --     servers = {
+    --         clangd = {
+    --             mason = false,
+    --         },
+    --     },
+    -- },
 }
