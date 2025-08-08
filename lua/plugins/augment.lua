cf3121725bf1a893f3a2d0e0b39947d78f1577eb
Gianluca Piccirillo <gianluca.piccirillo@mail.mcgill.ca>
2025-08-08 09:48:21 -0400

    remove augment code

--- lua/plugins/augment.lua
+++
@@ -1,8 +0,0 @@
-return {
-    "augmentcode/augment.vim",
-    config = function()
-        vim.api.nvim_set_keymap("n", "<leader>ac", ":Augment chat<CR>", { noremap = true, silent = true })
-        vim.api.nvim_set_keymap("n", "<leader>at", ":Augment chat-toggle<CR>", { noremap = true, silent = true })
-        vim.api.nvim_set_keymap("n", "<leader>an", ":Augment chat-new<CR>", { noremap = true, silent = true })
-    end,
-}
